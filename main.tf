b64f692cad96549d3bb479678e52bb874c4f710a
Umesh Ranasinghe <umesh@wso2telco.com>
2023-12-26 19:09:33 +0530

    remove name in sec_grp_rules and add sec_grp to sec_grp_rule

--- main.tf
+++ main.tf
@@ -35,7 +35,6 @@ resource "aws_security_group" "web" {
 }
 
 resource "aws_security_group_rule" "web_http_in" {
-  name = "http_in"
   type = "ingress"
   from_port = 80
   to_port = 80
@@ -46,16 +45,16 @@ resource "aws_security_group_rule" "web_http_in" {
 }
 
 resource "aws_security_group_rule" "web_https_in" {
-  name = "https_in"
   type = "ingress"
   from_port = 443
   to_port = 443
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
 }
 
 resource "aws_security_group_rule" "web_all_out" {
-  name = "web_all_out"
   type = "egress"
   from_port = 0
   to_port = 0
