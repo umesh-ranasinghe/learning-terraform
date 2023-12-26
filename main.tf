fe834a291e24b1144c5f73b86c8643ca89aaf248
Umesh Ranasinghe <umesh@wso2telco.com>
2023-12-26 18:41:56 +0530

    introduce security groups

--- main.tf
+++ main.tf
@@ -22,3 +22,45 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "web" {
+  name = "web"
+  description = "Allow http and https in. Allow all out."
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "web_http_in" {
+  name = "http_in"
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
+}
+
+resource "aws_security_group_rule" "web_https_in" {
+  name = "https_in"
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "web_all_out" {
+  name = "web_all_out"
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
+}
